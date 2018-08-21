b5a915258761216aad120d45a5bf65bc79ed379c
BouncyMaster <37939917+BouncyMaster@users.noreply.github.com>
2018-08-21 12:58:05 +0300

    Update ffmpeg-updater.py

--- ffmpeg-updater.py
+++ ffmpeg-updater.py
@@ -1,9 +1,11 @@
 from urllib.request import FancyURLopener
 from bs4 import BeautifulSoup
-from os import listdir, remove
+from os import listdir, remove, rename
 from shutil import rmtree
 from sys import path
 import zipfile
+
+ffmpeg_system = "win64-static" #you can change this to win32-static or macos64-static depending on your system
 
 class MyOpener(FancyURLopener): #ffmpeg site needs an User Agent to allow retrieving
 	version = "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36"
@@ -18,7 +20,7 @@ def check_new_ver():
 	
 	# kill all script and style elements
 	for script in soup(["script", "style"]):
-		script.extract()    # rip it out
+		script.extract() # rip it out
 	
 	# get text
 	text = soup.get_text()
@@ -29,17 +31,19 @@ def check_new_ver():
 def check_curr_ver():
 	global curr_ver
 	
-	dir_list = str(listdir("./"))
+	dir_list = str(listdir("./ffmpeg"))
 	ffmpeg_folder = dir_list.find("ffmpeg")
 	curr_ver = dir_list[ffmpeg_folder+7:ffmpeg_folder+23]
 
 def new_ver_install():
 	if new_ver != curr_ver:
-		myopener.retrieve("https://ffmpeg.zeranoe.com/builds/win64/static/ffmpeg-" + new_ver + "-win64-static.zip", "ffmpeg-" + new_ver + "-win64-static.zip") #You need to change win64-static to win32-static or macos64-static depending on your system
-		with zipfile.ZipFile("ffmpeg-" + new_ver + "-win64-static.zip","r") as zip_ref: #this also
+		myopener.retrieve("https://ffmpeg.zeranoe.com/builds/" + ffmpeg_system[:5] + "/" + ffmpeg_system[6:] + "/ffmpeg-" + new_ver + "-" + ffmpeg_system + ".zip", "ffmpeg-" + new_ver + "-" + ffmpeg_system + ".zip")
+		with zipfile.ZipFile("ffmpeg-" + new_ver + "-" + ffmpeg_system +".zip","r") as zip_ref:
 			zip_ref.extractall("./")
-		remove("ffmpeg-" + new_ver + "-win64-static.zip") #this also
-		rmtree("ffmpeg-" + curr_ver + "-win64-static") #this also
+		remove("ffmpeg-" + new_ver + "-" + ffmpeg_system + ".zip")
+		rmtree("ffmpeg")
+		rename("ffmpeg-" + new_ver + "-" + ffmpeg_system, "ffmpeg")
+		open("./ffmpeg/ffmpeg-" + new_ver + "-" + ffmpeg_system, "w+")
 
 if __name__ == "__main__":
 	check_new_ver()
